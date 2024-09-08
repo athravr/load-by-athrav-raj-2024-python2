65049150127311c3de5d51d77297067fe54ab20c
Athrav-coder <athravrajonwork@gmail.com>
2024-09-08 20:59:03 +0530

    Load.py

---
+++ Load.py
@@ -0,0 +1,42 @@
+#made by athrav raj
+print("made by athrav raj")
+from turtle import *
+turtle = Turtle()
+for i in range(4):
+  turtle.circle(10)
+for i in range(3):
+  turtle.forward(90)
+  turtle.left(90)
+turtle.forward(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
+turtle.forward(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
+turtle.forward(90)
+turtle.left(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
+turtle.width(50)
+turtle.forward(90)
+for i in range(4):
+  turtle.circle(10)
+for i in range(3):
+  turtle.forward(90)
+  turtle.left(90)
+turtle.forward(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
+turtle.forward(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
+turtle.forward(90)
+turtle.left(90)
+for i in range(3):
+  turtle.forward(90)
+  turtle.right(90)
